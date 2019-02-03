e75bb33c39b0021da1465db69dc545bee349d00f
Mohit Garg <mohit.garg@nagarro.com>
2019-02-03 12:23:43 +0530

    Add Handlers

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
+    constructor(props, context){
+        super(props, context);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(button_text){
+        if(this.props.quiz_question.answer === button_text){
+            this.props.showNextQuestionHandler();
+        }
+    }
     render() {
         return (
             <main>
@@ -10,7 +19,13 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {
+                            this.props.quiz_question.answer_options.map((answer_option, index) => {
+                                return <QuizQuestionButton key={index}
+                                 button_text={answer_option}
+                                 clickHandler={this.handleClick}/>
+                            })
+                        }
                     </ul>
                 </section>
             </main>
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component {
+    constructor(props, context){
+        super(props, context);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+        this.props.clickHandler(this.props.button_text);
+    }
     render() {
         return (
-           <li><button>{this.props.button_text}</button></li>
+           <li><button onClick={this.handleClick}>{this.props.button_text}</button></li>
         )
     }
 }
